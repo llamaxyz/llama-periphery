c6bf3cec730300a9b36d4296970cd52d1dcfa581
Rajath Alex <rajathalex@gmail.com>
2023-12-06 17:08:05 -0500

    refactor

--- src/token-voting/ERC20TokenholderActionCreator.sol
+++ src/token-voting/ERC20TokenholderActionCreator.sol
@@ -10,26 +10,39 @@ import {ERC20Votes} from "@openzeppelin/token/ERC20/extensions/ERC20Votes.sol";
 /// @notice This contract lets holders of a specified `ERC20Votes` token create actions on a llama instance if their
 /// token balance is greater than or equal to the creation threshold.
 contract ERC20TokenholderActionCreator is TokenholderActionCreator {
-  ERC20Votes public immutable TOKEN;
+  ERC20Votes public token;
 
-  constructor(ERC20Votes token, ILlamaCore llamaCore, uint256 _creationThreshold)
-    TokenholderActionCreator(llamaCore, _creationThreshold)
-  {
-    TOKEN = token;
-    uint256 totalSupply = TOKEN.totalSupply();
+  /// @dev This contract is deployed as a minimal proxy from the factory's `deployTokenVotingModule` function. The
+  /// `_disableInitializers` locks the implementation (logic) contract, preventing any future initialization of it.
+  constructor() {
+    _disableInitializers();
+  }
+
+  /// @notice Initializes a new `ERC20TokenholderActionCreator` clone.
+  /// @dev This function is called by the `deployTokenVotingModule` function in the `LlamaTokenVotingFactory` contract.
+  /// The `initializer` modifier ensures that this function can be invoked at most once.
+  /// @param _token The ERC20 token to be used for voting.
+  /// @param _llamaCore The `LlamaCore` contract for this Llama instance.
+  /// @param _creationThreshold The default number of tokens required to create an action. This must
+  /// be in the same decimals as the token. For example, if the token has 18 decimals and you want a
+  /// creation threshold of 1000 tokens, pass in 1000e18.
+  function initialize(ERC20Votes _token, ILlamaCore _llamaCore, uint256 _creationThreshold) external initializer {
+    __initializeTokenholderActionCreatorMinimalProxy(_llamaCore, _creationThreshold);
+    token = _token;
+    uint256 totalSupply = token.totalSupply();
     if (totalSupply == 0) revert InvalidTokenAddress();
     if (_creationThreshold > totalSupply) revert InvalidCreationThreshold();
   }
 
   function _getPastVotes(address account, uint256 timestamp) internal view virtual override returns (uint256) {
-    return TOKEN.getPastVotes(account, timestamp);
+    return token.getPastVotes(account, timestamp);
   }
 
   function _getPastTotalSupply(uint256 timestamp) internal view virtual override returns (uint256) {
-    return TOKEN.getPastTotalSupply(timestamp);
+    return token.getPastTotalSupply(timestamp);
   }
 
   function _getClockMode() internal view virtual override returns (string memory) {
-    return TOKEN.CLOCK_MODE();
+    return token.CLOCK_MODE();
   }
 }
