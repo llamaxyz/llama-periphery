b3e13e85a5235ef660719f756c6d3cdb76096f38
Rajath Alex <rajathalex@gmail.com>
2023-12-07 19:24:28 -0500

    cleanup

--- test/token-voting/LlamaTokenVotingTestSetup.sol
+++ test/token-voting/LlamaTokenVotingTestSetup.sol
@@ -4,9 +4,6 @@ pragma solidity ^0.8.23;
 import {Test, console2} from "forge-std/Test.sol";
 import {Vm} from "forge-std/Vm.sol";
 
-import {ERC20Votes} from "@openzeppelin/token/ERC20/extensions/ERC20Votes.sol";
-import {ERC721Votes} from "@openzeppelin/token/ERC721/extensions/ERC721Votes.sol";
-
 import {MockERC20Votes} from "test/mock/MockERC20Votes.sol";
 import {MockERC721Votes} from "test/mock/MockERC721Votes.sol";
 import {LlamaPeripheryTestSetup} from "test/LlamaPeripheryTestSetup.sol";
@@ -24,11 +21,9 @@ contract LlamaTokenVotingTestSetup is LlamaPeripheryTestSetup, DeployLlamaTokenV
   uint256 public constant ERC721_MIN_APPROVAL_PCT = 1;
   uint256 public constant ERC721_MIN_DISAPPROVAL_PCT = 1;
 
-  // Tokens
-  MockERC20Votes public mockERC20Votes;
-  ERC20Votes public erc20VotesToken;
-  MockERC721Votes public mockERC721Votes;
-  ERC721Votes public erc721VotesToken;
+  // Votes Tokens
+  MockERC20Votes public erc20VotesToken;
+  MockERC721Votes public erc721VotesToken;
 
   function setUp() public virtual override {
     LlamaPeripheryTestSetup.setUp();
@@ -37,20 +32,18 @@ contract LlamaTokenVotingTestSetup is LlamaPeripheryTestSetup, DeployLlamaTokenV
     DeployLlamaTokenVotingFactory.run();
 
     // Deploy the ERC20 and ERC721 tokens.
-    mockERC20Votes = new MockERC20Votes();
-    erc20VotesToken = ERC20Votes(address(mockERC20Votes));
-    mockERC721Votes = new MockERC721Votes();
-    erc721VotesToken = ERC721Votes(address(mockERC721Votes));
+    erc20VotesToken = new MockERC20Votes();
+    erc721VotesToken = new MockERC721Votes();
 
     // Mint tokens to core team members.
-    mockERC20Votes.mint(coreTeam1, 100);
-    mockERC20Votes.mint(coreTeam2, 100);
-    mockERC20Votes.mint(coreTeam3, 100);
-    mockERC20Votes.mint(coreTeam4, 100);
-    mockERC721Votes.mint(coreTeam1, 0);
-    mockERC721Votes.mint(coreTeam2, 1);
-    mockERC721Votes.mint(coreTeam3, 2);
-    mockERC721Votes.mint(coreTeam4, 3);
+    erc20VotesToken.mint(coreTeam1, 100);
+    erc20VotesToken.mint(coreTeam2, 100);
+    erc20VotesToken.mint(coreTeam3, 100);
+    erc20VotesToken.mint(coreTeam4, 100);
+    erc721VotesToken.mint(coreTeam1, 0);
+    erc721VotesToken.mint(coreTeam2, 1);
+    erc721VotesToken.mint(coreTeam3, 2);
+    erc721VotesToken.mint(coreTeam4, 3);
 
     // Mine block so that the ERC20 and ERC721 supply will be available when doing a past timestamp check.
     mineBlock();
