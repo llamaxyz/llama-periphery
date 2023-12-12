592ae18b5c43f7b68b076a0de40826ac8020ff82
dd0sxx <contacttheotelonis@gmail.com>
2023-12-11 22:38:18 -0500

    interface

--- src/token-voting/time/LlamaTimestampTimeManager.sol
+++ src/token-voting/time/LlamaTimestampTimeManager.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
 import {LlamaTokenVotingTimeManager} from "src/token-voting/time/LlamaTokenVotingTimeManager.sol";
--- src/token-voting/time/LlamaTokenVotingTimeManager.sol
+++ src/token-voting/time/LlamaTokenVotingTimeManager.sol
@@ -1,16 +1,17 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
 /// @title LlamaTokenVotingTimeManager
 /// @author Llama (devsdosomething@llama.xyz)
 /// @notice This contract provides a base contract and interface for time modules to inherit that works with the Llama
 /// token voting module.
-abstract contract LlamaTokenVotingTimeManager {
+interface LlamaTokenVotingTimeManager {
   /// @notice returns the most recent timepoint in the past.
-  function currentTimepointMinusOne() external view virtual returns (uint256 timepoint);
+  function currentTimepointMinusOne() external view returns (uint256 timepoint);
   /// @notice converts a timestamp to a timepoint units.
   /// @param timestamp The timestamp to convert.
-  function timestampToTimepoint(uint256 timestamp) external view virtual returns (uint256 timepoint);
+  function timestampToTimepoint(uint256 timestamp) external view returns (uint256 timepoint);
   /// @notice returns true if the clock mode is supported.
   /// @param clockMode The clock mode to check.
-  function isClockModeSupported(string memory clockMode) external pure virtual returns (bool);
+  function isClockModeSupported(string memory clockMode) external pure returns (bool);
 }
