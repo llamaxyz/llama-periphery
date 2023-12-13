2e88f66bb9d59c8994e383f88b21a3db66270d86
dd0sxx <contacttheotelonis@gmail.com>
2023-12-13 00:15:06 -0500

    Update src/interfaces/ILlamaTokenClockAdapter.sol
    Co-authored-by: Austin Green <austinzgreen@gmail.com>

--- src/interfaces/ILlamaTokenClockAdapter.sol
+++ src/interfaces/ILlamaTokenClockAdapter.sol
@@ -10,7 +10,7 @@ interface ILlamaTokenClockAdapter {
   /// @notice converts a timestamp to timepoint units.
   /// @param timestamp The timestamp to convert.
   function timestampToTimepoint(uint256 timestamp) external view returns (uint256 timepoint);
-  /// @notice returns true if the clock mode is supported.
+  /// @notice Returns true if the clock mode is supported and false if it is unsupported.
   /// @param clockMode The clock mode to check.
   function isClockModeSupported(string memory clockMode) external pure returns (bool);
 }
