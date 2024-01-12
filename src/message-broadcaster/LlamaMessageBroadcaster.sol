918bbda4f03e939daac90c28497282ead9db238d
Rajath Alex <rajathalex@gmail.com>
2024-01-12 15:10:36 -0500

    duck testing

--- src/message-broadcaster/LlamaMessageBroadcaster.sol
+++ src/message-broadcaster/LlamaMessageBroadcaster.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
+import {ILlamaCore} from "src/interfaces/ILlamaCore.sol";
+import {ILlamaExecutor} from "src/interfaces/ILlamaExecutor.sol";
+
 /// @title LlamaMessageBroadcaster
 /// @author Llama (devsdosomething@llama.xyz)
 /// @notice This contract enables Llama instances to broadcast an offchain message.
 contract LlamaMessageBroadcaster {
-  /// @dev Emitted when a message is broadcast.
-  event LlamaInstanceMessageBroadcasted(address indexed llamaExecutor, string message);
+  /// @dev Emitted when a message is broadcast by a Llama instance.
+  event MessageBroadcasted(ILlamaExecutor indexed llamaExecutor, string message);
 
   function broadcastMessage(string calldata message) external {
-    emit LlamaInstanceMessageBroadcasted(msg.sender, message);
+    ILlamaExecutor llamaExecutor = ILlamaExecutor(msg.sender);
+    // Duck testing to check if the caller is a Llama instance.
+    ILlamaCore(llamaExecutor.LLAMA_CORE()).actionsCount();
+    emit MessageBroadcasted(llamaExecutor, message);
   }
 }
