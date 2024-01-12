56e4cae5bb0098475c15fc607f2cee7be4634b4b
Rajath Alex <rajathalex@gmail.com>
2024-01-12 16:18:56 -0500

    integration test

--- test/message-broadcaster/LlamaMessageBroadcaster.t.sol
+++ test/message-broadcaster/LlamaMessageBroadcaster.t.sol
@@ -8,8 +8,9 @@ import {LlamaPeripheryTestSetup} from "test/LlamaPeripheryTestSetup.sol";
 
 import {DeployLlamaTokenVotingFactory} from "script/DeployLlamaTokenVotingFactory.s.sol";
 
-import {ILlamaCore} from "src/interfaces/ILlamaCore.sol";
 import {ILlamaExecutor} from "src/interfaces/ILlamaExecutor.sol";
+import {ILlamaPolicy} from "src/interfaces/ILlamaPolicy.sol";
+import {ActionInfo} from "src/lib/Structs.sol";
 import {LlamaMessageBroadcaster} from "src/message-broadcaster/LlamaMessageBroadcaster.sol";
 
 contract LlamaMessageBroadcasterTest is LlamaPeripheryTestSetup, DeployLlamaTokenVotingFactory {
@@ -30,6 +31,42 @@ contract BroadcastMessage is LlamaMessageBroadcasterTest {
     emit MessageBroadcasted(EXECUTOR, message);
     vm.prank(address(EXECUTOR));
     llamaMessageBroadcaster.broadcastMessage(message);
+  }
+
+  function test_BroadcastMessageFullActionLifecycle() public {
+    string memory message = "Hello World!";
+
+    // Giving Action Creator permission to call `LlamaMessageBroadcaster.broadcastMessage`.
+    vm.prank(address(EXECUTOR));
+    POLICY.setRolePermission(
+      CORE_TEAM_ROLE,
+      ILlamaPolicy.PermissionData(
+        address(llamaMessageBroadcaster), LlamaMessageBroadcaster.broadcastMessage.selector, address(STRATEGY)
+      ),
+      true
+    );
+
+    // Create Action to broadcast message.
+    bytes memory data = abi.encodeCall(LlamaMessageBroadcaster.broadcastMessage, (message));
+    vm.prank(coreTeam1);
+    uint256 actionId = CORE.createAction(CORE_TEAM_ROLE, STRATEGY, address(llamaMessageBroadcaster), 0, data, "");
+    ActionInfo memory actionInfo =
+      ActionInfo(actionId, coreTeam1, CORE_TEAM_ROLE, STRATEGY, address(llamaMessageBroadcaster), 0, data);
+
+    vm.warp(block.timestamp + 1);
+
+    // Approval and auto-queue process.
+    vm.prank(coreTeam2);
+    CORE.castApproval(CORE_TEAM_ROLE, actionInfo, "");
+    vm.prank(coreTeam3);
+    CORE.castApproval(CORE_TEAM_ROLE, actionInfo, "");
+    vm.prank(coreTeam4);
+    CORE.castApproval(CORE_TEAM_ROLE, actionInfo, "");
+
+    // Execute Action.
+    vm.expectEmit();
+    emit MessageBroadcasted(EXECUTOR, message);
+    CORE.executeAction(actionInfo);
   }
 
   function test_RevertIf_CallerIsNotExecutor() public {
